fd1a7571bf537bc3101725b228074b6fb1768665
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-16 10:26:57 +0100

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,20 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    #creates new unsaved, unvalidated Post object
+    @author = Author.new(author_params)
+    #Run the validations witout trying to save to the database
+    #returns true if valid
+    if @author.valid?
+      #if valid save item
+      @author.save
+      #return status code 302
+      #redirects user to instance of the post
+      redirect_to author_path(@author)
+    else
+      #it is invalid and now full of useful error messages to hold on to and re-render :new page, which know how to display error messages along side user's entries.
+      render :new
+    end
   end
 
   private
